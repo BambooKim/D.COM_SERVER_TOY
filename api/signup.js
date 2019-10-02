9a66674384fa5d2fa03671d8999e84daad490b95
bambooKim <rlaqjarn1008@khu.ac.kr>
2019-10-03 02:59:20 +0900

    Implement Signup Processing API

--- api/signup.js
+++ api/signup.js
@@ -6,11 +6,82 @@ exports.Signup = (req, res) => {
     const name = req.body.name
     const numId = req.body.numId
     const dept = req.body.dept
+    const userId = req.body.userId
     const pw = req.body.pw
     const email = req.body.email
 
-    console.log(name, numId, dept, pw, email)
+    const UserCheck = async () => {
+        try {
+            const connection = await pool.getConnection(async conn => conn)
+            try {
+                $query = "SELECT * FROM members WHERE NumId = ? || UserId = ?"
+                const [rows] = await connection.query($query, [numId, userId])
+                connection.release()
 
-    res.send("test")
-    res.end()
+                if (rows[0]) {
+                    return Promise.reject({
+                        code: "user_already_exists",
+                        message: "User already exists"
+                    })
+                } else {
+                    return Promise.resolve()
+                }
+            } catch (err) {
+                console.log(err)
+                return Promise.reject({
+                    code:'database_query_error',
+                    message:'database_query_error'
+                })
+            }
+        } catch (err) {
+            console.log(err)
+            return Promise.reject({
+                code:'database_connection_error',
+                message:'Failed to connect database'
+            })
+        }
+    }
+
+    const Create = async () => {
+        const doCreate = async () => {
+            try {
+                const connection = await pool.getConnection(async conn => conn)
+                try {
+                    $query = "INSERT INTO members (Name, NumId, Department, UserId, Password, email) VALUES (?, ?, ?, ?, ?, ?)"
+                    const result = await connection.query($query, [name, numId, dept, userId, pw, email])
+                    connection.release()
+                    return Promise.resolve()
+                } catch (err) {
+                    console.log(err)
+                    return Promise.reject({
+                        code:'database_error',
+                        message:'Database error'
+                    })
+                }
+            } catch (err) {
+                console.log(err)
+                return Promise.reject({
+                    code:'database_error',
+                    message:'Database error'
+                })
+            }
+        }
+
+        doCreate()
+    }
+
+    UserCheck()
+    .then(Create)
+    .then(() => {
+        console.log(name, numId, dept, userId, pw, email)
+
+        res.send("Success")
+        res.end()
+    })
+    .catch((err) => {
+        console.log(err)
+
+        res.send(err.code)
+        res.end()
+    })
 }
